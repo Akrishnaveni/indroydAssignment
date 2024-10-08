867113f3248cf84161cf0cb03691341ca2c16704
Akrishnaveni <annakrishnaveni05@gmail.com>
2024-10-08 19:15:28 +0000

    fixing deployment warnings

--- src/MobileScreen.js
+++ src/MobileScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Redirect } from "react-router-dom";
+
 import "./MobileScreen.css";
 
 const MobileScreen = () => {
